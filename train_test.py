076fcf0a6b4ca3b443d33a29f78a9f215abbc4b5
Vishwajit Sasi <vishwajit123@users.noreply.github.com>
2016-10-12 11:26:13 +0530

    make train and test

--- train_test.py
+++ train_test.py
@@ -3,9 +3,11 @@ import ebow
 import numpy as np
 from sklearn.neural_network import MLPClassfier
 
+
 labels=np.load('score_train.npy')
 feats= np.load('train_feats.npy')
 
 NN = MLPClassifier(solver='lbfgs', alpha=1e-5, \
             hidden_layer_sizes=(8, 4), random_state=1)
-NN.fit(X, labels)
+NN.fit(feats, labels)
+
