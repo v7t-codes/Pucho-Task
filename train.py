306c704545015916684e71ec8c7cb7b752ed754b
Vishwajit Sasi <vishwajit123@users.noreply.github.com>
2016-10-12 11:20:54 +0530

    Curated data

--- train.py
+++ train.py
@@ -3,15 +3,9 @@ import ebow
 import numpy as np
 from sklearn.neural_network import MLPClassfier
 
-
 labels=np.load('score_train.npy')
-feats= np.load('train_features.npy')
-feats= np.delete(feats,0)  #removing the header vector
-X=[]
-for i in feats:
-    i=np.array(i)
-    X.append(i)
-X= np.array(X)   #X is the input feature 2D vector input to the classifier
+feats= np.load('train_feats.npy')
+
 NN = MLPClassifier(solver='lbfgs', alpha=1e-5, \
             hidden_layer_sizes=(8, 4), random_state=1)
 NN.fit(X, labels)
