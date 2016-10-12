d6aa1bcd7d61ff722cb45a1d180f5f14a2f253b8
Vishwajit Sasi <vishwajit123@users.noreply.github.com>
2016-10-12 14:24:43 +0530

    Hopefully final

--- train_test.py
+++ train_test.py
@@ -3,11 +3,23 @@ import ebow
 import numpy as np
 from sklearn.neural_network import MLPClassfier
 
-
-labels=np.load('score_train.npy')
-feats= np.load('train_feats.npy')
+labels=np.load('/home/psi/PuchoTask/train_score.npy')
+feats= np.load('/home/psi/PuchoTask/train_feats.npy')
 
 NN = MLPClassifier(solver='lbfgs', alpha=1e-5, \
-            hidden_layer_sizes=(8, 4), random_state=1)
+            hidden_layer_sizes=(5, 10, 4), random_state=1)
+
 NN.fit(feats, labels)
 
+test_feats = np.load('/home/psi/PuchoTask/test_feats.npy')
+test_ids = np.load('/home/psi/PuchoTask/test_ids.npy')
+
+prediction = []
+
+for i in test_feats:
+    x = NN.predict(i)
+    prediction.append(x[0])
+
+prediction = np.array(prediction)
+prediction = prediction.astype(np.int)
+np.save('prediction.npy', prediction)
