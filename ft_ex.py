2bbc23017c541282b83ea856f5c5993d6e2ca4ba
Vishwajit Sasi <vishwajit123@users.noreply.github.com>
2016-10-22 21:36:16 +0530

    NEW FEATURES NOT USED ,BAD GRADIENTS

--- ft_ex.py
+++ ft_ex.py
@@ -10,12 +10,14 @@ The following are the features used from the dataset:
 2.Unique words
 3.Average word length
 4.Spelling errors
-5.Commas
+5.Commas in essay
 6.Essay set
 7.Correctly spelled words
 8.Number of sentences
 9.Number of double quotes
-10.Unique parts of speech tags count
+10.Unique parts of speech tags count ----takes over 4 hours so dropped
+11.No. of words per sentence
+12.Average number syllables in a word
 
 """
 
@@ -39,27 +41,27 @@ def avr_word_len(bow):
     for i in bow:
         num+=len(i)
     avr_wrd_len = num/den
-    return avr_wrd_len
+    return round(float(avr_wrd_len),3)
 
 #To count the total number of words
 def no_words(bow):
     count= 0
     for i in bow:
         count+=bow[i]
-    return count
+    return float(count)
 
 #To count the number of unique words
 def no_unq_words(bow):
-    return len(bow)
+    return float(len(bow))
 
 #To count the number of commas from the essay string
 def count_commas_dquots(string):
-    return string.count(','), string.count('"')
+    return float(string.count(',')), float(string.count('"'))
 
 #To count the number of spelling errors and the correctly spelled words in an essay
 def count_spell_error(string):
     string = ((((string.replace(".", " ")).replace(","," ")).replace("?"," ")).replace('!',' '))
-    string = nltk.word_tokenize(string)
+    string = nltk.word_tokenize(string.strip(":;"))
     check = False
     error = 0
     correct = 0
@@ -70,23 +72,54 @@ def count_spell_error(string):
             correct+=1
         else :
             error+=1
-    return error, correct
+    return float(error), float(correct)
 
 # To count the number unique of POS tags
 def POS_count(string):
     x = []
-    words = nltk.word_tokenize(string.split(' '))
+    words = nltk.word_tokenize(string)
     tagged = nltk.pos_tag(words)
-    for i in tagged:
-        _, tag = i
-        x.append(tag)
-    x = set(x)       #creates a dict with only the unique tags
-    return len(x)
+    x = [i[1] for i in tagged]
+    x = set(x)      #creates a dict with only the unique tags
+    del words
+    del tagged
+    return float(len(x))
 
-# To count the number of sentences
+#To count the number of sentences
 def sent_count(string):
     cnt = len(string.split('.'))
     return cnt
+
+#To count the number of syllables in a word
+def syllables(word):
+    count = 0
+    vowels = 'aeiou'
+    word = word.lower().strip(".:;?!")
+    if word[0] in vowels:
+        count +=1
+    for index in range(1,len(word)):
+        if word[index] in vowels and word[index-1] not in vowels:
+            count +=1
+    if word.endswith('e'):
+        count -= 1
+    if word.endswith('le'):
+        count+=1
+    if count == 0:
+        count +=1
+    return count
+
+def av_sylls(string):
+    string = ((((string.replace(".", " ")).replace(","," ")).replace("?"," ")).replace('!',' '))
+    words = nltk.word_tokenize(string)
+    y = len(words)
+    val = 0.0
+    for j in words:
+        if len(j)>1:
+            val+= int(syllables(j))
+    if y == 0:
+        return 1.0
+    x = val/y
+    return round(x,3)
 
 def extract_feats(essays,sets):
     features = [[]]
@@ -94,16 +127,19 @@ def extract_feats(essays,sets):
     i=0
     for essay in essays:
         bow = ebow.essay_to_bow(essay)
-        f1 = int(no_words(bow))
-        f2 = int(no_unq_words(bow))
-        f3 = int(avr_word_len(bow))
-        f4, f7 = int(count_spell_error(essay))
-        f5 ,f9= int(count_commas_dquots(essay))
+        f1 = no_words(bow)
+        f2 = no_unq_words(bow)
+        f3 = avr_word_len(bow)
+        f4, f7 = count_spell_error(essay)
+        f5 ,f9= count_commas_dquots(essay)
         f6 = int(sets[i])
-        f8 = int(sent_count(essay))
-        f10 = int(POS_count(essay))
-        f = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10]
+        f8 = sent_count(essay)
+        #f10 = POS_count(essay)
+        f11 = round(f1/f8, 3)
+        f12 = av_sylls(essay)
+        f = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f11, f12]
         f = np.array(f)
+        f = f.astype(np.float)
         features.append(f)
         i+=1
     features = np.array(features)
